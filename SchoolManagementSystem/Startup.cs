cb95e50173f07c25155927dc4c83d914d57c4ed9
indradey <deyindranil@outlook.com>
2017-12-17 21:46:12 +0530

    No change

--- SchoolManagementSystem/Startup.cs
+++ SchoolManagementSystem/Startup.cs
@@ -14,7 +14,7 @@ namespace SchoolManagementSystem
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
