22fdbe06004594865c2a8bfc1ea6d2833bfd7d57
indradey <deyindranil@outlook.com>
2017-12-17 21:43:24 +0530

    Made modification for Entityframework from MongoDB

--- SchoolManagementSystem/Models/Entities/Parent.cs
+++ SchoolManagementSystem/Models/Entities/Parent.cs
@@ -1,9 +1,17 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace SchoolManagementSystem.Models.Entities
 {
     public class Parent
     {
+        [Key]
         public int ParentID { get; set; }
-        public string StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        public string Address { get; set; }
+        public int RecordStatus { get; set; }
     }
 }
--- SchoolManagementSystem/Models/Entities/Student.cs
+++ SchoolManagementSystem/Models/Entities/Student.cs
@@ -7,5 +7,13 @@ namespace SchoolManagementSystem.Models.Entities
 {
     public class Student
     {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        public string Address { get; set; }
+        public string Comments { get; set; }
+        public int RecordStatus { get; set; }
     }
 }
